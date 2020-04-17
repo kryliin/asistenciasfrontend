3c025133de65c1695de679288adb7ddd72262658
saliwonczykc <saliwonczykc@frcu.utn.edu.ar>
2020-04-17 11:26:03 -0300

    Initial commit

---
+++ scripts/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('todoListApp')
+    .controller('mainCtrl', function ($scope, dataService) {
+        $scope.addTodo = function () {
+            dataService.addTodo(function (response) {
+                $scope.todos.unshift(response.data);
+            });
+        };
+
+        dataService.getTodos(function (response) {
+            $scope.todos = response.data;
+        });
+
+        $scope.deleteTodo = function (todo, $index) {
+            dataService.deleteTodo(todo, function (response) {
+                $scope.todos.splice($index, 1);
+            });
+        };
+
+        $scope.saveTodo = function (todo, $index) {
+            dataService.saveTodo(todo, function (response) {
+                console.log(response.data)
+            });
+        };
+    })
